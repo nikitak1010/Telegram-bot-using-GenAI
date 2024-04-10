05e533e5aa1708971a3ff31d2e45a68f74e6def6
nikitak1010 <nikitak1010>
2024-04-10 21:24:20 +0530

    updated

--- research/my_bot.py
+++ research/my_bot.py
@@ -18,4 +18,27 @@ MODEL_NAME = "gpt-3.5-turbo"
 
 #Initialize bot
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dispatcher = Dispatcher(bot)
+
+
+class Reference:
+    def __init__(self) -> None:
+        self.response = ""
+
+
+reference = Reference()
+
+def clear_past():
+    reference.response = ""
+
+@dispatcher.message_handler(commands=['start'])
+async def command_start_handler(message: types.Message):
+    """This handler receives messages with `/start` or `/help` command
+    Args:
+        message (types.Message): __descriptions__
+    """
+    await message.reply("Hi!\n I am a MRSPTU College Bot. How can i assist you?")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dispatcher, skip_updates=True)
